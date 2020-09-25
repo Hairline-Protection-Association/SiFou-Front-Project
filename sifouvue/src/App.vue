ada7d77f71619a828c22be4bc344e2571a31b4c1
looiker <591221298@qq.com>
2020-09-25 21:22:31 +0800

    SecondCommit

---
+++ sifouvue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ sifouvue/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test">
+    <Card style="width: 300px; margin: auto;">
+        <div style="text-align:center">
+            <img src="../assets/logo.png">
+            <h3>A high quality UI Toolkit based on Vue.js</h3>
+        </div>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+  #loginbox{
+      margin: 30px ,auto;
+  }
+  .test{
+    background-color: pink;
+  }
+</style>

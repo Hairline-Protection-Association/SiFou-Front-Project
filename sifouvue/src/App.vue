85ee4df693f137bea6aed01b050f8ed3bc1c3533
looiker <591221298@qq.com>
2020-09-26 14:21:22 +0800

    login

--- sifouvue/src/App.vue
+++ sifouvue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
@@ -12,6 +12,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  #app{
+      height: 100%;
+  }
 </style>
--- sifouvue/src/router/index.js
+++ sifouvue/src/router/index.js
@@ -5,6 +5,9 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/', redirect: '/login'
+  },
+  {
     path: '/home',
     name: 'Home',
     component: () => import('../views/Home.vue')
